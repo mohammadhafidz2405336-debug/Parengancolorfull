6e357946b19174680c3eb070cdba3e3cda931273
HafidzSuhaiby <79145498+HafidzSuhaiby@users.noreply.github.com>
2026-06-04 14:50:24 +0700

    Fix: force asset to use HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Pastikan baris ini ada
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Tambahkan kondisi ini agar jika di Railway (production), asset dipaksa ke HTTPS
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
