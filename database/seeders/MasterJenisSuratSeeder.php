f1410fd2914514b2b0adf03975a1f37b3e5b185c
HafidzSuhaiby <79145498+HafidzSuhaiby@users.noreply.github.com>
2026-06-27 23:18:23 +0700

    Mengubah insert menjadi insertOrIgnore pada MasterJenisSuratSeeder

--- database/seeders/MasterJenisSuratSeeder.php
+++ database/seeders/MasterJenisSuratSeeder.php
@@ -12,8 +12,8 @@ class MasterJenisSuratSeeder extends Seeder
      */
     public function run(): void
     {
-        // Pastikan nama tabel di bawah ini sesuai dengan tabel master di databasemu
-        DB::table('master_jenis_surat')->insert([
+        // Menggunakan insertOrIgnore agar data yang sudah ada diabaikan dan tidak memicu error duplicate key
+        DB::table('master_jenis_surat')->insertOrIgnore([
             [
                 'id' => 1,
                 'nama_surat' => 'Pembuatan Akta Nikah',
@@ -64,7 +64,7 @@ class MasterJenisSuratSeeder extends Seeder
             ],
             [
                 'id' => 9,
-                'nama_surat' => 'Pembuatan SKTM',
+                'nama_surat' => 'Pembuatan SKTM', // Sesuaikan jika ada perbedaan nama surat ke-9
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
